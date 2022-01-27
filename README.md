5e4d9ea883f63351b67748f0bb3687ff737d38a4
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2022-01-27 06:59:27 +0100

    feat: vue component lib - Integrated with Lerna package - Refactor project structure #15

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# verida-vue-components
+# sb-test
 
-Open Source Vue Components for Verida
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your unit tests
+```
+npm run test:unit
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ components/account/dev/serve.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import VdaAccount from "@/vda-account.vue";
+
+export default defineComponent({
+  name: "ServeDev",
+  components: {
+    VdaAccount,
+  },
+  data() {
+    return {
+      contextName: "Verida: Account Component",
+      logo: "https://assets.verida.io/verida_login_request_logo_170x170.png",
+    };
+  },
+  methods: {
+    onSuccess(response: any) {
+      console.log("App Context", response);
+    },
+    onError(error: Error) {
+      console.log("Login Error", error);
+    },
+    onLogout() {},
+  },
+});
+</script>
+
+<template>
+  <div id="app">
+    <vda-account
+      :logo="logo"
+      :contextName="contextName"
+      :onError="onError"
+      :onSuccess="onSuccess"
+    />
+  </div>
+</template>
